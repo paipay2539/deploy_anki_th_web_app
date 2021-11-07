cbbbb59f29ef8c846c788133e7b891ae845c543e
paidev2539 <paidev2539@gmail.com>
2021-11-07 13:10:10 +0700

    commit

--- anki_th_web_app_project/settings.py
+++ anki_th_web_app_project/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-BASE_DIR = os.path.dirname(PROJECT_ROOT)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -55,7 +54,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'anki_th_web_app_project.urls'
 
-
+import os
 
 TEMPLATES = [
     {
