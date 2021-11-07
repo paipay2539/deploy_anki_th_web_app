8e5675d7d5bc0ce94f7adef4ecf3427d712e74e0
paidev2539 <paidev2539@gmail.com>
2021-11-07 15:24:49 +0700

    commit

--- anki_th_web_app_project/settings.py
+++ anki_th_web_app_project/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'anki_th_web_app_project.urls'
