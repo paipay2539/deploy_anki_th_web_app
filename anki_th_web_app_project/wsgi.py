3158496374f6cf7c96f055612ab9f65ea95eef22
paidev2539 <paidev2539@gmail.com>
2021-11-07 13:41:52 +0700

    commit

--- anki_th_web_app_project/wsgi.py
+++ anki_th_web_app_project/wsgi.py
@@ -19,4 +19,3 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
