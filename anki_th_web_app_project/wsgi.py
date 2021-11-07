59e95d730f0e43cb80d12b3524c983dc638144c6
paidev2539 <paidev2539@gmail.com>
2021-11-07 13:39:49 +0700

    commit

--- anki_th_web_app_project/wsgi.py
+++ anki_th_web_app_project/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anki_th_web_app_project.settings')
 
-application = get_wsgi_application()
+
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
