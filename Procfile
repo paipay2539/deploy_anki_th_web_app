6a868016ad7a7bff8de5883e41f80fb7724c5b41
paidev2539 <paidev2539@gmail.com>
2021-11-07 14:24:57 +0700

    commit

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: gunicorn anki_th_web_app_project.wsgi --log-file -
 config: set DISABLE_COLLECTSTATIC=1
 config: set DEBUG_COLLECTSTATIC=1
+web: python manage.py collectstatic --no-input; gunicorn myapp.wsgi --log-file - --log-level debug
--- anki_th_web_app_project/settings.py
+++ anki_th_web_app_project/settings.py
@@ -13,11 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
-
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -84,8 +80,7 @@ WSGI_APPLICATION = 'anki_th_web_app_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
