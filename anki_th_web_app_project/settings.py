edd0d5f55483a57443e3ecdbbf0b609e02d03ea9
paidev2539 <paidev2539@gmail.com>
2021-11-07 14:17:51 +0700

    commit

--- anki_th_web_app_project/settings.py
+++ anki_th_web_app_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-%tow39--f49*@q^))0&-7btv=izfa1pwbt82)7mct&tsnn2c#%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["localhost", "192.168.1.104", '127.0.0.1', 'anki-th-web-app.herokuapp.com',]
