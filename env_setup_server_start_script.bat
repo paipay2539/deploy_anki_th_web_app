bf93a38f9791ed63d3b3bb8ff739afba92ec714e
paidev2539 <paidev2539@gmail.com>
2021-11-07 15:36:22 +0700

    commit

--- env_setup_server_start_script.bat
+++ env_setup_server_start_script.bat
@@ -1,2 +1,2 @@
 cd %CD%\project_env
-cmd /k "Scripts\activate && cd .. && cd /d anki_th_web_app_project & python manage.py runserver 0.0.0.0:8000 && cd .."
+cmd /k "Scripts\activate && cd .. && python manage.py runserver 0.0.0.0:8000 && cd .."
